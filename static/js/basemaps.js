33c18c2a990c528edec196a7baeae8ee10fa1bf1
jclark <jclark@xylemgeo.com>
2023-03-02 19:25:33 -0500

    split story overlays into partials

---
+++ static/js/basemaps.js
@@ -0,0 +1,13 @@
+var Basemaps_map = {
+	// cont:{},
+	init: function(){
+		if(typeof map.legend !== 'undefined'){
+			map.removeControl(map.legend)
+		}
+        // var layerControl = L.control.layers(layers)
+        // this.cont = map.addControl(new layerControl());
+	}
+
+
+}
+
