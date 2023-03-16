95a8875e311c9f487aa58548057f10b4a61e413a
jclark <jclark@xylemgeo.com>
2023-03-16 17:50:29 -0400

    implement leaflet side-by-side

--- static/js/split_map.js
+++ static/js/split_map.js
@@ -1,13 +1,26 @@
 var Split_map = {
 	// cont:{},
-	init: function(){
-
-		var osmLayer = L.tileLayer('https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}', {
+	init: function(section){
+		leftmap = $('.'+section).attr('leftmap')
+		rightmap = $('.'+section).attr('rightmap')
+		$('.layers').each(function(ex){
+			if($(this).attr('layer-id')==leftmap){
+				leftmapurl = $(this).attr('layer-src')
+			}
+			if($(this).attr('layer-id')==rightmap){
+				rightmapurl = $(this).attr('layer-src')
+			}
+		})
+		var leftLayer = L.tileLayer(leftmapurl, {
 		}).addTo(map);
-	
-		var stamenLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		}).addTo(map)
-	
-		L.control.sideBySide(stamenLayer, osmLayer).addTo(map);
+		var rightLayer = L.tileLayer(rightmapurl, {
+		}).addTo(map);
+		sbs = L.control.sideBySide(leftLayer, rightLayer)
+		sbs.addTo(map);
+	},
+	remove: function(){
+		if(sbs){
+			map.removeControl(sbs)
+		}
 	}
 }
